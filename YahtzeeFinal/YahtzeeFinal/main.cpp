f0a4b5255aa9941c6b16e5305639e364ba6f461f
Heidi Wiebers <hwiebers19@hotmail.com>
2016-11-15 13:19:49 -0800

    initial

---
+++ YahtzeeFinal/YahtzeeFinal/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  YahtzeeFinal
+//
+//  Created by Heidi Wiebers on 11/15/16.
+//  Copyright © 2016 Heidi Wiebers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
